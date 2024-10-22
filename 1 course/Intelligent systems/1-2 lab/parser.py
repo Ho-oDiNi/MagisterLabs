daa434b6b73feed88a9ed2a3652abc80e21764d2
Ho-oDiNi <evgen5060@gmail.com>
2024-10-22 16:28:47 +0300

    refactor: add comments and other

--- 1 course/Intelligent systems/1-2 lab/parser.py
+++ 1 course/Intelligent systems/1-2 lab/parser.py
@@ -5,6 +5,7 @@ import pymorphy3
 from pymystem3 import Mystem
 import csv
 import re
+import os
 
 # Заполняем БД
 def createTables(dbFileName):
@@ -13,7 +14,8 @@ def createTables(dbFileName):
     
     cur.execute("CREATE TABLE db_urlList("
                 "id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                "url TEXT)"
+                "url TEXT, "
+                "isIndexed INTEGER DEFAULT 0)"
     )
 
     cur.execute("CREATE TABLE db_wordList("
@@ -61,12 +63,25 @@ def deleteTables(dbFileName):
 
     return
 
+def createCsv(CSVfilename):
+    with open(CSVfilename, 'a', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(["UrlListLen", "linkBetweenURLLen", "fltWorlListLen"])
+
 # Паук
 class Crawler:
 
     # 0. Конструктор Инициализация паука с параметрами БД
-    def __init__(self, dbFileName, mainUrl):
-        self.db = sqlite3.connect(dbFileName)
+    def __init__(self, mainUrl):
+
+        self.path = os.path.dirname(os.path.abspath(__file__))
+
+        self.dbFileName = os.path.join(self.path, 'crawl.db')
+        self.CSVfilename = os.path.join(self.path, 'db_Inserting.csv')
+
+        self.db = sqlite3.connect(self.dbFileName)
+
+        # createCsv(self.CSVfilename)
         # createTables(self.db)
         
         self.mainUrl = mainUrl
@@ -76,110 +91,26 @@ class Crawler:
 
         return
 
+
+
     # 0. Деструктор
     def __del__(self):
         # deleteTables(self.db)
         return
 
 
-    # 1. Индексирование одной страницы
-    def addIndex(self, soup, url):
-        if (self.isIndexed(url)):  #  если страница уже проиндексирована, то ее не индексируем isIndexed
-            return
-
-        print(f"Indexing: {url}")
-
-        textList = self.getTextOnly(soup)
-        
-        if textList == None:
-            return
-
-        url_id = self.getEntryId('db_urlList', 'url', url)  #   Получаем идентификатор URL getEntryId
-        
-        location = 0
-        for word in textList:
-            location = location + 1
-            word_id = self.getEntryId('db_wordList', 'word', word)
-            word_location_id = self.getEntryId('db_wordLocation', 'fk_url_id', url_id, 'fk_word_id', word_id, 'location', location) 
-
-        return
-
-
-    # 2. Получение текста страницы
-    def getTextOnly(self, soup):
-        
-        textList = []
-        if (soup.find('div', class_='post__content') == None):
-            return
-        
-        pMain = soup.find('div', class_='post__content').find_all(['p', 'blockquote'])
-
-        for data in pMain:
-            if data.text == '':
-                continue
-            textList.append(data.text)
-
-        for i in range(len(textList)):
-            textList[i] = self.textFilter(textList[i]) 
-       
-        newTextList = self.doubleArrayToSingle(textList)
-
-        return newTextList
-
-    # Удалить все кроме букв и цифр
-    def clearText(self, text):
-        return re.sub(r'[\W ]+', ' ', text)
-
-    # 3. Разбиение текста на слова
-    def separateWords(self, text):
-        newClearText = self.clearText(text)
-        return newClearText.split()
-
-    # 4. Проиндексирован ли URL (проверка наличия URL в БД)
-    def isIndexed(self, url):
-        
-        cur = self.db.cursor()
-        
-        request = cur.execute(f"SELECT id FROM db_urlList WHERE url = '{url}' ").fetchone()
-
-        if(request != None): 
-            request = cur.execute(f"SELECT fk_word_id FROM db_wordLocation WHERE fk_url_id = '{request}' ").fetchone()
-            
-            if(request != None):
-                return True
-            
-        return False
- 
-
-    # 5. Добавление ссылки с одной страницы на другую
-    def addLinkRef(self, urlFrom:str, urlTo:str, linkText:str):
-        
-        cur = self.db.cursor()
-
-        urlFrom_id = self.getEntryId('db_urlList', 'url', urlFrom)
-        urlTo_id = self.getEntryId('db_urlList', 'url', urlTo)
-
-        link_id = self.getEntryId('db_linkBetweenURL', 'fk_fromURL_id', urlFrom_id, 'fk_toURL_id', urlTo_id)        
-
-        filteredTextLink = self.textFilter(linkText)
-
-        for word in filteredTextLink:
-            word_id = self.getEntryId('db_wordList', 'word', word)
-            link_word_id = self.getEntryId('db_linkWord', 'fk_link_id', link_id, 'fk_word_id', word_id)
-
-        self.db.commit()
-
-
-    # 6. Непосредственно сам метод сбора данных.
-    # Начиная с заданного списка страниц, выполняет поиск в ширину
-    # до заданной глубины, индексируя все встречающиеся по пути страницы
+    # Непосредственно сам метод сбора данных.
     def crawl(self, urlList, maxDepth=1):
 
+        # Обход каждого url на текущей глубине
         for currDepth in range(0, maxDepth):
-        #     # Вар.1. обход каждого url на текущей глубине
+            
             newUrlList = []
             for url in urlList:
-                
+
+                if(self.isIndexed(url)):
+                    continue
+
                 html_doc = requests.get(url).text   # получить HTML-код страницы по текущему url    
                 soup = BeautifulSoup(html_doc, "html.parser")   # использовать парсер для работа тегов
             
@@ -200,43 +131,72 @@ class Crawler:
         return  
 
 
-    # 7. Вспомогательная функция для получения идентификатора и
-    # добавления записи, если такой еще нет
-    def getEntryId(self, tableName, fieldNameFirst, valueFirst, fieldNameSecond = False, valueSecond = False, fieldNameThird = False, valueThird = False):
+    ############################################################
+    #########            Все о индексации              ######### 
+    ############################################################
+
+    # Индексирование одной страницы
+    def addIndex(self, soup, url):
+        if (self.isIndexed(url)):  #  если страница уже проиндексирована, то ее не индексируем isIndexed
+            return
+
+        print(f"Indexing: {url}")
+
+        fltTextList = self.getTextOnly(soup)
+        
+        url_id = self.getEntryId('db_urlList', 'url', url)  #   Получаем идентификатор URL
+        self.addTextToDB(url_id, fltTextList)
+        self.urlUpdateIndexed(url_id)
+
+        return
+
+
+    # Проиндексирован ли URL (проверка наличия URL в БД)
+    def isIndexed(self, url):
+        
+        cur = self.db.cursor()
+        request = cur.execute(f"SELECT isIndexed FROM db_urlList WHERE url = '{url}' ").fetchone()
+        if(request): 
+            if(request[0]):
+                return True
+            
+        return False
+ 
+    # Страница была проиндексирована
+    def urlUpdateIndexed(self, urlId):
         cur = self.db.cursor()
 
-        fieldNameList = [fieldNameSecond, fieldNameThird]
-        valueList = [valueSecond, valueThird]
+        requestUpdate = f"UPDATE db_urlList SET isIndexed = 1 WHERE id = '{urlId}'"
+        cur.execute(requestUpdate)
 
-        requestGet = f"SELECT id FROM '{tableName}' WHERE {fieldNameFirst} = '{valueFirst}'"
-        
-        for i in range(len(valueList)):
-            if valueList[i]:
-                requestGet+= f" AND {fieldNameList[i]} = '{valueList[i]}'"
-        
-        request_id = cur.execute(f"{requestGet}").fetchone()
-
-        if request_id != None:
-            return request_id[0]
-
-        requestInsert = f"INSERT INTO '{tableName}' ({fieldNameFirst}"
-
-        for i in range(len(valueList)):
-            if valueList[i]:
-                requestInsert += f", {fieldNameList[i]}"
-        requestInsert += f") VALUES ('{valueFirst}'"
-
-        for i in range(len(valueList)):
-            if valueList[i]:
-                requestInsert += f", '{valueList[i]}'"
-        requestInsert += f")"
-
-        cur.execute(f"{requestInsert}")
         self.db.commit()
 
-        request_id = cur.execute(f"{requestGet}").fetchone()
 
-        return request_id[0]
+
+
+    ############################################################
+    #########              Все о ссылках               ######### 
+    ############################################################
+
+
+
+    # Добавление ссылки с одной страницы на другую
+    def addLinkRef(self, urlFrom:str, urlTo:str, linkText:str):
+        
+        cur = self.db.cursor()
+
+        urlFrom_id = self.getEntryId('db_urlList', 'url', urlFrom)
+        urlTo_id = self.getEntryId('db_urlList', 'url', urlTo)
+
+        link_id = self.getEntryId('db_linkBetweenURL', 'fk_fromURL_id', urlFrom_id, 'fk_toURL_id', urlTo_id)        
+
+        textLink = self.textFilter(linkText)
+
+        for word in textLink:
+            word_id = self.getEntryId('db_wordList', 'word', word)
+            link_word_id = self.getEntryId('db_linkWord', 'fk_link_id', link_id, 'fk_word_id', word_id)
+
+        self.db.commit()
 
 
     # Форматируем ссылку - Очищаем ссылку от ненужный хуков и тд
@@ -275,15 +235,56 @@ class Crawler:
             return newfilteredLinks, newtextLinks
 
 
-    # Двумерный массив в одномерный
-    def doubleArrayToSingle(self, doubleArray):
-        singleArray = []
-        for arr in doubleArray:
-            for element in arr:
-                singleArray.append(element)
-            
-        return singleArray
 
+    ############################################################
+    #########              Все о тексте                ######### 
+    ############################################################
+
+
+    # Получение текста страницы
+    def getTextOnly(self, soup):
+        
+        textList = []
+
+        if (soup.find('div', class_='post__content') == None):
+            return None, None
+        
+        pMain = soup.find('div', class_='post__content').find_all(['p', 'blockquote'])
+
+        for data in pMain:
+            if data.text == '':
+                continue
+            textList.append(data.text)
+
+        for i in range(len(textList)):
+            textList[i] = self.textFilter(textList[i]) 
+       
+        filteredTextList = self.doubleArrayToSingle(textList)
+
+        return filteredTextList
+        
+
+
+    # Удалить все кроме букв и цифр
+    def clearText(self, text):
+        return re.sub(r'[\W ]+', ' ', text)
+
+    # Разбиение текста на слова
+    def separateWords(self, text):
+        newClearText = self.clearText(text)
+        return newClearText.split()
+
+    # Добавление слов в БД
+    def addTextToDB(self, url_id, fltTextList):
+        
+        if fltTextList != None:
+            location = 0
+            for word in fltTextList:
+                location = location + 1
+                word_id = self.getEntryId('db_wordList', 'word', word)
+                word_location_id = self.getEntryId('db_wordLocation', 'fk_url_id', url_id, 'fk_word_id', word_id, 'location', location) 
+
+        return
 
     # Удалить все союзы междометия и тд
     def pos(self, word):
@@ -311,25 +312,85 @@ class Crawler:
 
         return newText
 
-    def writeCsv(self, worlListLen, linkBetweenURLLen, UrlListLen, filename = "db_Inserting.csv"):
-        with open(filename, 'a', newline='') as csvfile:
-            spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            spamwriter.writerow([worlListLen, linkBetweenURLLen, UrlListLen])
 
-    def check_db(self):
+    ############################################################
+    #########              Доп. Функции                ######### 
+    ############################################################
+
+    # Вспомогательная функция для получения идентификатора и
+    # добавления записи, если такой еще нет
+    def getEntryId(self, tableName, fieldNameFirst, valueFirst, fieldNameSecond = False, valueSecond = False, fieldNameThird = False, valueThird = False):
         cur = self.db.cursor()
 
-        requestGetWorlList = f"SELECT * FROM db_wordList"
+        fieldNameList = [fieldNameSecond, fieldNameThird]
+        valueList = [valueSecond, valueThird]
+
+        requestGet = f"SELECT id FROM '{tableName}' WHERE {fieldNameFirst} = '{valueFirst}'"
+        
+        for i in range(len(valueList)):
+            if valueList[i]:
+                requestGet+= f" AND {fieldNameList[i]} = '{valueList[i]}'"
+        
+        request_id = cur.execute(f"{requestGet}").fetchone()
+
+        if request_id != None:
+            return request_id[0]
+
+        requestInsert = f"INSERT INTO '{tableName}' ({fieldNameFirst}"
+
+        for i in range(len(valueList)):
+            if valueList[i]:
+                requestInsert += f", {fieldNameList[i]}"
+        requestInsert += f") VALUES ('{valueFirst}'"
+
+        for i in range(len(valueList)):
+            if valueList[i]:
+                requestInsert += f", '{valueList[i]}'"
+        requestInsert += f")"
+
+        cur.execute(f"{requestInsert}")
+        self.db.commit()
+
+        request_id = cur.execute(f"{requestGet}").fetchone()
+
+        return request_id[0]
+
+
+    # Двумерный массив в одномерный
+    def doubleArrayToSingle(self, doubleArray):
+        singleArray = []
+        for arr in doubleArray:
+            for element in arr:
+                singleArray.append(element)
+            
+        return singleArray
+
+
+    # Записать разультаты работы в CSV
+    def writeCsv(self, UrlListLen, linkBetweenURLLen, fltWorlListLen):
+        with open(self.CSVfilename, 'a', newline='') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            spamwriter.writerow([UrlListLen, linkBetweenURLLen, fltWorlListLen])
+
+
+    # Проверить количество записей в БД
+    def check_db(self):
+        cur = self.db.cursor()
+       
         requestGetLinkBetweenURL = f"SELECT * FROM db_linkBetweenURL"
         requestGetUrlList = f"SELECT * FROM db_urlList"
+        requestGetFLTWorlList = f"SELECT * FROM db_wordList"
 
-        worlListLen = len(cur.execute(f"{requestGetWorlList}").fetchall())
-        linkBetweenURLLen = len(cur.execute(f"{requestGetLinkBetweenURL}").fetchall())
+
         UrlListLen = len(cur.execute(f"{requestGetUrlList}").fetchall())
+        linkBetweenURLLen = len(cur.execute(f"{requestGetLinkBetweenURL}").fetchall())
+        worlListLen = len(cur.execute(f"{requestGetFLTWorlList}").fetchall())
 
-        self.writeCsv(worlListLen, linkBetweenURLLen, UrlListLen)
+        
+        self.writeCsv(UrlListLen, linkBetweenURLLen, worlListLen)
 
         return
+        
 
 
 def main():
@@ -340,7 +401,7 @@ def main():
         url_page = url + '/page/' + str(i)
         urlList.append(url_page)
     
-    crawler = Crawler('crawl.db', url)
+    crawler = Crawler(url)
     crawler.crawl(urlList, maxDepth=2)
 
 
